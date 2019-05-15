935d5f31bf8e00d20c14c19faaa3962c649f296b
Akhila <kakhila96@gmail.com>
2019-05-15 09:23:51 +0530

    sixth task completed

---
+++ src/main/java/com/stackroute/spring/bean/PostProcessor.java
@@ -0,0 +1,21 @@
+package com.stackroute.spring.bean;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class PostProcessor implements BeanPostProcessor {
+
+    public java.lang.Object postProcessBeforeInitialization(java.lang.Object bean, java.lang.String beanName) throws org.springframework.beans.BeansException {
+            System.out.println("In post processorBefore Initialisation of :"+beanName);
+            return bean;
+        }
+
+
+        public java.lang.Object postProcessAfterInitialization(java.lang.Object bean, java.lang.String beanName) throws org.springframework.beans.BeansException {
+            System.out.println("In post Aftetr Initialisation :"+beanName);
+            return bean;
+
+        }
+
+
+    }
+
